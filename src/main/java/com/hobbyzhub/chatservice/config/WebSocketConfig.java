dda731381a7876494b12e7f34f75e644b6cb453d
Q-T5 <bikathimartin@outlook.com>
2023-02-16 13:42:48 +0300

    Add channel interceptor configuration

--- src/main/java/com/hobbyzhub/chatservice/config/WebSocketConfig.java
+++ src/main/java/com/hobbyzhub/chatservice/config/WebSocketConfig.java
@@ -1,15 +1,21 @@
 package com.hobbyzhub.chatservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import com.hobbyzhub.chatservice.listener.WebSockectChannelInterceptor;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
+	@Autowired
+	WebSockectChannelInterceptor channelInterceptor;
+	
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws-registry").setAllowedOriginPatterns("*").withSockJS();
@@ -27,4 +33,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 		
 		registry.setApplicationDestinationPrefixes("/app");
 	}
+
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.interceptors(channelInterceptor);
+	}
+	
+	
 }
