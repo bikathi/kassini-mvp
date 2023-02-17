ad47e02e78a81bfd3f6ac044b1fe326bf3f0af12
Q-T5 <bikathimartin@outlook.com>
2023-02-17 22:53:33 +0300

    Add service methods to connect to the repository

--- src/main/java/com/hobbyzhub/chatservice/service/PrivateMessageStoreService.java
+++ src/main/java/com/hobbyzhub/chatservice/service/PrivateMessageStoreService.java
@@ -1,12 +1,31 @@
 package com.hobbyzhub.chatservice.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.hobbyzhub.chatservice.entity.PrivateMessageStore;
 import com.hobbyzhub.chatservice.repository.PrivateMessageStoreRepository;
 
 @Service
 public class PrivateMessageStoreService {
 	@Autowired
 	PrivateMessageStoreRepository storeRepository;
+	
+	public void saveNewChatStore(PrivateMessageStore messageStore) {
+		storeRepository.save(messageStore);
+	}
+	
+	public boolean chatStoreExists(String chatId) {
+		return storeRepository.existsById(chatId);
+	}
+	
+	public Optional<PrivateMessageStore> getMessageStore(String chatId) {
+		return storeRepository.findById(chatId);
+	}
+	
+	public void updateChatStore(PrivateMessageStore updatedStore) {
+		storeRepository.save(null);
+	}
 }
