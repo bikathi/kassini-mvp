4ef9be48e58b6b1bb2b59096901da884df30528e
Q-T5 <bikathimartin@outlook.com>
2023-02-17 20:22:39 +0300

    Added MongoDB structure to store group and private chat messages

--- src/main/java/com/hobbyzhub/chatservice/entity/GroupMessageStore.java
+++ src/main/java/com/hobbyzhub/chatservice/entity/GroupMessageStore.java
@@ -2,12 +2,13 @@ package com.hobbyzhub.chatservice.entity;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import com.hobbyzhub.chatservice.dto.PrivateMessageDTO;
+import com.hobbyzhub.chatservice.dto.GroupMessageDTO;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,36 +19,37 @@ import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 @Builder
 
-@Document(collection = "private_chats")
-public class PrivateMessageStore implements Serializable {
+@Document(collection = "group_chats")
+public class GroupMessageStore implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	private String chatId;
+	private String groupId;
 	private String dateInitiated;
 	private Long messageCount;
 	
 	@Builder.Default
-	private List<PrivateMessageDTO> messages = new ArrayList<>();
+	private List<GroupMessageDTO> messages = new LinkedList<>();
 	
 	// convenience methods
-	public boolean addMessage(PrivateMessageDTO newMessage) {
+	public boolean addMessage(GroupMessageDTO newMessage) {
 		return this.messages.add(newMessage);
 	}
 	
-	public List<PrivateMessageDTO> getMessageList(Integer startIndex, int endIndex) {
+	public List<GroupMessageDTO> getMessageList(Integer startIndex, int endIndex) {
 		if(this.messages.isEmpty() != Boolean.TRUE) {
-			ArrayList<PrivateMessageDTO> messagesList = 
-				(ArrayList<PrivateMessageDTO>) this.messages.subList(startIndex, endIndex + 1);
+			ArrayList<GroupMessageDTO> messagesList = 
+				(ArrayList<GroupMessageDTO>) this.messages.subList(startIndex, endIndex + 1);
 			
 			return messagesList; 
 		} else {
 			return new ArrayList<>();
 		}
 	}
+	
 }
