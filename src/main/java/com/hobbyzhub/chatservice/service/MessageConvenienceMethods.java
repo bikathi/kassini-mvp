fa3e36423145561b14762323d413e89d58f68eff
Q-T5 <bikathimartin@outlook.com>
2023-02-18 15:09:25 +0300

    Improve on how the messages are saved to message list

--- src/main/java/com/hobbyzhub/chatservice/service/MessageConvenienceMethods.java
+++ src/main/java/com/hobbyzhub/chatservice/service/MessageConvenienceMethods.java
@@ -3,34 +3,40 @@ package com.hobbyzhub.chatservice.service;
 import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
 
 import com.hobbyzhub.chatservice.dto.GroupMessageDTO;
 import com.hobbyzhub.chatservice.dto.PrivateMessageDTO;
+import com.hobbyzhub.chatservice.entity.GroupMessageStore;
 import com.hobbyzhub.chatservice.entity.PrivateMessageStore;
 
+@Service
 public class MessageConvenienceMethods {
 	@Autowired
-	static PrivateMessageStoreService privateMessageStoreService;
+	PrivateMessageStoreService privateMessageStoreService;
 	
-	public static void addPrivateMessageToStore(PrivateMessageDTO message) {
-		// then save the message to the chat store
+	@Autowired
+	GroupMessageStoreService groupMessageStoreService;
+	
+	public void addPrivateMessageToStore(PrivateMessageDTO message) {
         String chatId = message.getFromUserId() + message.getToUserId();
+        
         // if the chat store already exists, just add this message to the list of messages
         // and update the message count
         if(privateMessageStoreService.chatStoreExists(chatId) == (Boolean.TRUE)) {
-        	PrivateMessageStore tempChatStore = privateMessageStoreService.getMessageStore(chatId).get();
+        	Query query = new Query().addCriteria(Criteria.where("_id").is(chatId));
         	
-        	tempChatStore.addMessage(
+        	PrivateMessageDTO messageToStore = 
     			PrivateMessageDTO.builder()
     			.message(message.getMessage())
     			.dateSent(message.getDateSent())
-    			.build()
-			);
+    			.build();
+        	Update updateDefinition = new Update().push("messages", messageToStore);
         	
-        	// increase message count
-        	tempChatStore.increaseMessageCount();
-        	
-        	privateMessageStoreService.updateChatStore(tempChatStore);
+        	privateMessageStoreService.updateChatStore(query, updateDefinition, PrivateMessageStore.class);
     	// else if the chat store does not exists(the chat is therefore new), create a new store
     	// and add this message to the store
         } else {
@@ -52,7 +58,41 @@ public class MessageConvenienceMethods {
         }
 	}
 	
-	public static void addGroupMessageToStore(String chatId, GroupMessageDTO message) {
+	public void addGroupMessageToStore(GroupMessageDTO message) {
+		// the chatId is similar to the groupId
+		String chatId = message.getToGroupId();
 		
+		if(groupMessageStoreService.chatStoreExists(chatId) == Boolean.TRUE) {
+			Query query = new Query().addCriteria(Criteria.where("_id").is(chatId));
+			
+			// structure of the message we're storing drops the toGroupId variable
+			GroupMessageDTO messageToStore = GroupMessageDTO.builder()
+				.fromUserId(message.getFromUserId())
+				.message(message.getMessage())
+				.dateSent(message.getDateSent())
+				.build();
+			Update updateDefinition = new Update().push("messages", messageToStore);
+			
+			groupMessageStoreService.updateChatStore(query, updateDefinition, GroupMessageStore.class);
+		// else if the chat store does not exist(the chat is therefore new), create a new store
+    	// and add this message to the store
+		} else {
+			GroupMessageStore newMessageStore = 
+				GroupMessageStore.builder()
+				.chatId(chatId)
+				.dateInitiated(LocalDate.now().toString())
+				.messageCount(1L)
+				.build();
+			
+			newMessageStore.addMessage(
+				GroupMessageDTO.builder()
+				.fromUserId(message.getFromUserId())
+				.message(message.getMessage())
+				.dateSent(message.getDateSent())
+				.build()
+			);
+			
+			groupMessageStoreService.saveNewChatStore(newMessageStore);
+		}
 	}
 }
