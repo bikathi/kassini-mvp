4aaef54ead82bac8b59e8ad136af082a924e93b7
Q-T5 <bikathimartin@outlook.com>
2023-02-19 22:53:03 +0300

    Add a structure for the messages sent from the internal system

--- src/main/java/com/hobbyzhub/chatservice/service/DestinationManagementService.java
+++ src/main/java/com/hobbyzhub/chatservice/service/DestinationManagementService.java
@@ -6,7 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.jms.TextMessage;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
@@ -17,9 +25,17 @@ public class DestinationManagementService {
 	
 	public boolean createPrivateDestination(String userId) {
 		try {
+			String internalMessage = new ObjectMapper().writer().withDefaultPrettyPrinter()
+				.writeValueAsString(
+					InternalMessageSource.builder()
+					.id("HobbyzHub")
+					.dateSent(LocalDate.now().toString())
+					.message("Welcome to HobbyzHub. We're glad to have you.")
+					.build());
+			
 			jmsTemplate.send("user-" + userId, messageCreator -> {
                 TextMessage deliverable = messageCreator.createTextMessage();
-                deliverable.setText("Testing user's destination availability");
+                deliverable.setText(internalMessage);
                 return deliverable;
             });
 			
@@ -33,11 +49,18 @@ public class DestinationManagementService {
 	
 	public boolean createGroupDestination(String groupId) {
 		try {
+			String dateToday = LocalDate.now().toString();
+			String internalMessage = new ObjectMapper().writer().withDefaultPrettyPrinter()
+				.writeValueAsString(
+					InternalMessageSource.builder()
+					.id("HobbyzHub")
+					.dateSent(dateToday)
+					.message("Group Created On: " + dateToday)
+					.build());
+			
 			jmsTemplate.send("group-" + groupId, messageCreator -> {
 	            TextMessage deliverable = messageCreator.createTextMessage();
-	            
-	            String dateToday = LocalDate.now().toString();
-	            deliverable.setText(String.format("Group created on: {}", dateToday));
+	            deliverable.setText(internalMessage);
 	            return deliverable;
 	        });
 			
@@ -47,5 +70,17 @@ public class DestinationManagementService {
 			log.error("JMSTemplate error creating group destination: {}", ex.getMessage());
 			return Boolean.FALSE;
 		}
+	}
+	
+	@Getter
+	@Setter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@ToString
+	@Builder
+	static class InternalMessageSource {
+		private String id;
+		private String message;
+		private String dateSent;
 	}
 }
