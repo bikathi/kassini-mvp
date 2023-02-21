4f273c9ce0acc016cfcae42aa28c92f39c5e0ed0
Q-T5 <bikathimartin@outlook.com>
2023-02-21 13:03:56 +0300

    Add package to hold service payloads

---
+++ src/main/java/com/hobbyzhub/chatservice/payload/request/JoinNewChatRequest.java
@@ -0,0 +1,21 @@
+package com.hobbyzhub.chatservice.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JoinNewChatRequest {
+	private String chatId;
+	private String chatType; // group or private chat
+	private Boolean chatActive; // is the chat blocked(false) or active(true)
+	private String chatName;
+}
