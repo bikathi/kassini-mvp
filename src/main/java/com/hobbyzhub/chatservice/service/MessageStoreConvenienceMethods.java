04f146efd3c3d4394d5052ce911836b272fa5e76
Q-T5 <bikathimartin@outlook.com>
2023-05-03 15:19:08 +0300

    Add classes to house storage convenience methods

---
+++ src/main/java/com/hobbyzhub/chatservice/service/MessageStoreConvenienceMethods.java
@@ -0,0 +1,14 @@
+package com.hobbyzhub.chatservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageStoreConvenienceMethods {
+    public void storePrivateMessage() {
+
+    }
+
+    public void storeGroupChatMessage() {
+
+    }
+}
