39eb8294afc6f4ca29ffa6e1781f59f45f874ce8
Q-T5 <bikathimartin@outlook.com>
2023-05-03 23:16:32 +0300

    Add logic to test updating of group name

--- src/main/java/com/hobbyzhub/chatservice/controller/ChatModelController.java
+++ src/main/java/com/hobbyzhub/chatservice/controller/ChatModelController.java
@@ -3,6 +3,7 @@ package com.hobbyzhub.chatservice.controller;
 import com.hobbyzhub.chatservice.entity.ChatModel;
 import com.hobbyzhub.chatservice.payload.request.NewGroupChatModelRequest;
 import com.hobbyzhub.chatservice.payload.request.NewPrivateChatModelRequest;
+import com.hobbyzhub.chatservice.payload.request.ChangeGroupChatModelNameRequest;
 import com.hobbyzhub.chatservice.payload.response.GenericServiceResponse;
 import com.hobbyzhub.chatservice.service.ChatModelService;
 import com.hobbyzhub.chatservice.service.DestinationManagementService;
@@ -10,6 +11,10 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -98,7 +103,6 @@ public class ChatModelController {
                 .userId(newChatModelRequest.getFromUserId())
                 .userName(newChatModelRequest.getFromUserName())
                 .userProfilePicLink(newChatModelRequest.getFromUserProfilePicLink())
-                .isChatAdmin(true)
             .build()
         );
 
@@ -108,7 +112,6 @@ public class ChatModelController {
                 .userId(newChatModelRequest.getToUserId())
                 .userName(newChatModelRequest.getToUserName())
                 .userProfilePicLink(newChatModelRequest.getToUserProfilePicLink())
-                .isChatAdmin(true)
             .build()
         );
 
@@ -136,7 +139,24 @@ public class ChatModelController {
         }
     }
 
-    public ResponseEntity<?> updateGroupChatModelName() {
+    @PostMapping(value = "/group-chat/change-name")
+    public ResponseEntity<?> updateGroupChatModelName(@RequestBody ChangeGroupChatModelNameRequest updateNameRequest) {
+        Query findQuery = new Query().addCriteria(Criteria.where("chatId").is(updateNameRequest.getChatId()));
+        Update updateDefinition = new Update().set("chatName", updateNameRequest.getNewName());
+        FindAndModifyOptions findAndModifyOptions = new FindAndModifyOptions().returnNew(false).upsert(false);
+
+        try {
+            chatModelService.updateChatModelName(findQuery, updateDefinition, findAndModifyOptions, ChatModel.class);
+            log.info("Updated group chat name for Id: {}", updateNameRequest.getChatId());
+            return new ResponseEntity<>(new GenericServiceResponse<>(apiVersion, organizationName,
+                "Successfully updated chat name", HttpStatus.OK.value(), null),
+            HttpStatus.OK);
+        } catch(Exception ex) {
+            log.error("Error updating name for groupId: {}. Caused by: {}", updateNameRequest.getChatId(), ex.getMessage());
+            return new ResponseEntity<>(new GenericServiceResponse<>(apiVersion, organizationName,
+                "Error updating group name", HttpStatus.INTERNAL_SERVER_ERROR.value(), null),
+            HttpStatus.INTERNAL_SERVER_ERROR);
+        }
         return null;
     }
 
