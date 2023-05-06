f37ccb0fe801ef133ce6d3bf8014b4b00b827012
Q-T5 <bikathimartin@outlook.com>
2023-05-06 20:11:09 +0300

    Create aggregation to get paged participant list

--- src/main/java/com/hobbyzhub/chatservice/service/ChatModelService.java
+++ src/main/java/com/hobbyzhub/chatservice/service/ChatModelService.java
@@ -2,12 +2,19 @@ package com.hobbyzhub.chatservice.service;
 
 import com.hobbyzhub.chatservice.entity.ChatModel;
 import com.hobbyzhub.chatservice.repository.ChatModelRepository;
+import com.mongodb.client.model.geojson.LineString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.UpdateDefinition;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ChatModelService {
@@ -39,8 +46,27 @@ public class ChatModelService {
         chatModelTemplate.updateFirst(query, updateDefinition, entityClass);
     }
 
-    public void makeParticipantAdmin() {
+    public List<ChatModel.ChatParticipants> getPagedListOfParticipants(String chatId, Integer pageNumber, Integer pageSize) {
+        Query findQuery = new Query(Criteria.where("chatId").is(chatId));
+        ChatModel chatModel = chatModelTemplate.findOne(findQuery, ChatModel.class);
 
+        if(chatModel != null) {
+            Aggregation aggregationFunction = Aggregation.newAggregation(
+                Aggregation.match(Criteria.where("chatId").is(chatId)),
+                Aggregation.unwind("chatParticipants"),
+                Aggregation.skip((long) pageNumber * pageSize), // TODO : Test this to ensure it works
+                Aggregation.limit(pageSize)
+            );
+
+            AggregationResults<ChatModel.ChatParticipants> aggregationResults =
+                chatModelTemplate.aggregate(aggregationFunction, ChatModel.class, ChatModel.ChatParticipants.class);
+
+            List<ChatModel.ChatParticipants> requiredList = aggregationResults.getMappedResults();
+
+            return requiredList;
+        } else {
+            return new ArrayList<>();
+        }
     }
 
     public void deleteEntireChatModel(String chatModelId) {
