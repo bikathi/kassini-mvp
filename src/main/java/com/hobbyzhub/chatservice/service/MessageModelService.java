d6bbf660dc331f7db49fd37099e3b62179149755
Q-T5 <bikathimartin@outlook.com>
2023-05-09 20:30:15 +0300

    Add controller to retrieve list of chat messages

--- src/main/java/com/hobbyzhub/chatservice/service/MessageModelService.java
+++ src/main/java/com/hobbyzhub/chatservice/service/MessageModelService.java
@@ -1,11 +1,20 @@
 package com.hobbyzhub.chatservice.service;
 
+import com.hobbyzhub.chatservice.entity.MessageModel;
 import com.hobbyzhub.chatservice.repository.MessageModelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageModelService {
     @Autowired
     private MessageModelRepository messageModelRepository;
+
+    public Page<MessageModel> getPagedMessageList(Integer pageSize, Integer pageNumber, String chatId) {
+        Pageable page = PageRequest.of(pageNumber, pageSize);
+        return messageModelRepository.findAllByChatId(chatId, page);
+    }
 }
