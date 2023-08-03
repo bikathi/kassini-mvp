800715e78f343bfac29adfeb8ff0ca66a88d02ad
Bikathi Martin <bikathimartin@outlook.com>
2023-08-03 15:04:14 +0300

    Complete the User Details implementation

--- src/main/java/npc/kassinimvp/service/UserDetailsImpl.java
+++ src/main/java/npc/kassinimvp/service/UserDetailsImpl.java
@@ -1,11 +1,32 @@
 package npc.kassinimvp.service;
 
+import npc.kassinimvp.entity.AppUser;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
 public class UserDetailsImpl implements UserDetails {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Integer phoneNumber;
+
+    // custom constructors
+    public UserDetailsImpl(String firstName, String lastName, String email, String password, Integer phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public static UserDetailsImpl build(AppUser user) {
+        // we can handle the role conversion from Set<?> to List<?> here
+        return new UserDetailsImpl(user.getFirstName(), user.getLastName(), user.getEmail(), user.getPassword(), user.getPhoneNumber());
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
@@ -13,31 +34,39 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return firstName + " "  +lastName;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
