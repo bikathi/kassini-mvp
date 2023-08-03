ff7d7835d3d7b7188a2b48367c09570189de9711
Bikathi Martin <bikathimartin@outlook.com>
2023-08-03 15:05:13 +0300

    Complete logic to find users by their usernames(emails)

--- src/main/java/npc/kassinimvp/service/UserDetailsServiceImpl.java
+++ src/main/java/npc/kassinimvp/service/UserDetailsServiceImpl.java
@@ -1,5 +1,8 @@
 package npc.kassinimvp.service;
 
+import npc.kassinimvp.entity.AppUser;
+import npc.kassinimvp.repository.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +10,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    AppUserRepository appUserRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // in our case here we are actually loading user's by their email as and not the username
+        AppUser user = appUserRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User with email"  + email + "not found"));
+
+        return UserDetailsImpl.build(user);
     }
 }
