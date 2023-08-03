b69c5cb28c2ab8dcb59b1ebb2a0af5b89e7c5e9f
Bikathi Martin <bikathimartin@outlook.com>
2023-08-03 15:03:14 +0300

    Add custom method to find user by unique email

--- src/main/java/npc/kassinimvp/repository/AppUserRepository.java
+++ src/main/java/npc/kassinimvp/repository/AppUserRepository.java
@@ -4,6 +4,10 @@ import npc.kassinimvp.entity.AppUser;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AppUserRepository extends MongoRepository<AppUser, String> {
+    // a custom method to find user details of whoever's logging in by their email
+    Optional<AppUser> findByEmail(String email);
 }
