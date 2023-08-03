ef27d59ba429713a88a6a731499352542677c6ed
Bikathi Martin <bikathimartin@outlook.com>
2023-08-03 20:56:29 +0300

    Complete the web security confirguration file

--- src/main/java/npc/kassinimvp/security/WebSecurityConfig.java
+++ src/main/java/npc/kassinimvp/security/WebSecurityConfig.java
@@ -1,20 +1,30 @@
 package npc.kassinimvp.security;
 
-import npc.kassinimvp.security.service.UserDetailsImpl;
+import npc.kassinimvp.security.jwt.AuthEntryPointJwt;
+import npc.kassinimvp.security.jwt.AuthenticationTokenFilter;
+import npc.kassinimvp.security.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
     @Autowired
-    UserDetailsImpl userDetailsImpl;
+    UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Autowired
+    AuthEntryPointJwt unauthorizedHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -22,7 +32,37 @@ public class WebSecurityConfig {
     }
 
     @Bean
+    public AuthenticationTokenFilter authenticationTokenFilter() {
+        return new AuthenticationTokenFilter();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+
+        authenticationProvider.setUserDetailsService(userDetailsServiceImpl);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return null;
+        httpSecurity.csrf(csrf -> csrf.disable())
+            .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth ->
+            auth.requestMatchers("/api/v1/auth/**").permitAll()
+                .anyRequest().authenticated()
+            );
+
+        httpSecurity.authenticationProvider(daoAuthenticationProvider());
+        httpSecurity.addFilterBefore(authenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
     }
 }
