26672aab7c3bca33b56d236afe4e7b69933ae9fe
Bikathi Martin <bikathimartin@outlook.com>
2023-08-05 11:44:59 +0300

    Return relevant response for 401 or 403 HTTP status

--- src/main/java/npc/kassinimvp/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/npc/kassinimvp/security/jwt/AuthEntryPointJwt.java
@@ -1,9 +1,16 @@
 package npc.kassinimvp.security.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import npc.kassinimvp.payload.response.GenericServiceResponse;
+import npc.kassinimvp.payload.response.MessageResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -13,12 +20,32 @@ import java.io.IOException;
 @Slf4j
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
+    @Value("${service.api.version}")
+    private String apiVersion;
+
+    @Value("${service.organization.name}")
+    private String organizationName;
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        log.info("Exception type: {}", authException.getClass());
         log.error("Unauthorized error: {}", authException.getMessage());
 
-        // In this simple test case we are sending back to the client a simple 401 Unauthorized
-        // TODO: be sure to make this an actual full fledged response body
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+        final ObjectMapper mapper = new ObjectMapper();
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        if(authException.getClass() == InsufficientAuthenticationException.class) {
+            GenericServiceResponse<MessageResponse> permissionsResponse = new GenericServiceResponse<>(apiVersion, organizationName,
+                "Forbidden", HttpStatus.FORBIDDEN.value(), new MessageResponse("Insufficient permissions to make this request"));
+
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            mapper.writeValue(response.getOutputStream(), permissionsResponse);
+        }
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        GenericServiceResponse<MessageResponse> unauthorizedResponse = new GenericServiceResponse<>(apiVersion, organizationName,
+        "Unauthorized", HttpStatus.UNAUTHORIZED.value(), new MessageResponse("Invalid authorization credentials"));
+
+        mapper.writeValue(response.getOutputStream(), unauthorizedResponse);
     }
 }
