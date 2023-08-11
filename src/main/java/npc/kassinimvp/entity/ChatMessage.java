0ea45a90302ec530b18e57e92a33168ac007f748
Bikathi Martin <bikathimartin@outlook.com>
2023-08-11 21:23:17 +0300

    Declare class as a MongoDB entity with new chatId field

--- src/main/java/npc/kassinimvp/entity/ChatMessage.java
+++ src/main/java/npc/kassinimvp/entity/ChatMessage.java
@@ -2,14 +2,21 @@ package npc.kassinimvp.entity;
 
 import lombok.*;
 import npc.kassinimvp.entity.definitions.ProductTextDetails;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@Builder
+@Document(collection = "chat-messages")
 public class ChatMessage {
+    @Id
+    private String messageId;
     private String typeOfMessage; // in this case is 'text' or 'product'
+    private String chatId;
     private String message;
     private String fromUserId;
     private String toUserId;
