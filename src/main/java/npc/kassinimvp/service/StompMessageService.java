dc83048913d3ccefbe6b7057db2a811868eca03a
Bikathi Martin <bikathimartin@outlook.com>
2023-08-11 22:33:48 +0300

    Add logic to save messages to DB

--- src/main/java/npc/kassinimvp/service/StompMessageService.java
+++ src/main/java/npc/kassinimvp/service/StompMessageService.java
@@ -1,6 +1,8 @@
 package npc.kassinimvp.service;
 
+import npc.kassinimvp.entity.ChatMessage;
 import npc.kassinimvp.payload.request.ProductChatMessagePayload;
+import org.jgroups.demos.Chat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.jms.TextMessage;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -30,7 +34,19 @@ public class StompMessageService {
                 return deliverable;
             });
 
-            // TODO: then save it into the database
+            // then save it into the database
+            ChatMessage messageToSave = ChatMessage.builder()
+                .messageId(generateChatMessageId())
+                .typeOfMessage("text")
+                .chatId(message.getChatId())
+                .message(message.getMessage())
+                .fromUserId(message.getFromUserId())
+                .toUserId(message.getToUserId())
+                .dateSent(message.getDateSent())
+                .productTextDetails(null)
+            .build();
+            chatMessageService.saveChat(messageToSave);
+
             return true;
         } catch (Exception ex) {
             log.error("Encountered error trying to send private text message: {}", ex.getMessage());
@@ -47,11 +63,28 @@ public class StompMessageService {
                 return deliverable;
             });
 
-            // TODO: then save it into the database
+            // then save it into the database
+            ChatMessage messageToSave = ChatMessage.builder()
+                .messageId(generateChatMessageId())
+                .typeOfMessage("product")
+                .chatId(message.getChatId())
+                .message(message.getMessage())
+                .fromUserId(message.getFromUserId())
+                .toUserId(message.getToUserId())
+                .dateSent(message.getDateSent())
+                .productTextDetails(message.getProductTextDetails())
+            .build();
+            chatMessageService.saveChat(messageToSave);
+
             return true;
         } catch(Exception ex) {
             log.error("Encountered error trying to send private product message: {}", ex.getMessage());
             return false;
         }
+    }
+
+    private String generateChatMessageId() {
+        return UUID.randomUUID().toString()
+            .replace("-", "").substring(0, 20);
     }
 }
