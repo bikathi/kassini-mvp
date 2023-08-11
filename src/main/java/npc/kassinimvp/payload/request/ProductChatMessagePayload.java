8e285465d4d4a66a1975b2c68ff652d20d66a90a
Bikathi Martin <bikathimartin@outlook.com>
2023-08-11 20:05:44 +0300

    Extract static class to global public class

--- src/main/java/npc/kassinimvp/payload/request/ProductChatMessagePayload.java
+++ src/main/java/npc/kassinimvp/payload/request/ProductChatMessagePayload.java
@@ -1,22 +1,12 @@
 package npc.kassinimvp.payload.request;
 
 import lombok.*;
+import npc.kassinimvp.entity.definitions.ProductTextDetails;
 
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
 public class ProductChatMessagePayload extends TextChatMessagePayload {
-    @Getter
-    @Setter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @ToString
-    public static class ProductDetails {
-        private String productImage;
-        private String postId;
-        private String productName;
-        private String vendorName;
-        private String productDescription;
-    }
+    private ProductTextDetails productTextDetails;
 }
