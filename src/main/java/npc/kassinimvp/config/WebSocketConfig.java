74dd6dbf2c5c1ae927986cfea7055e9732100e5a
Bikathi Martin <bikathimartin@outlook.com>
2023-08-12 19:28:53 +0300

    Add support for pure websocket connection without SockJS

--- src/main/java/npc/kassinimvp/config/WebSocketConfig.java
+++ src/main/java/npc/kassinimvp/config/WebSocketConfig.java
@@ -5,24 +5,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@EnableWebSocket
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Autowired
     WebSockectChannelInterceptor channelInterceptor;
 	
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		// for normal websocket connections
+		registry.addEndpoint("/ws-registry").setAllowedOriginPatterns("*");
+
+		// for other web-socket connections that support SockJS fallback
 		registry.addEndpoint("/ws-registry").setAllowedOriginPatterns("*").withSockJS();
 	}
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		registry.enableStompBrokerRelay("/queue/")
+		registry.enableStompBrokerRelay("/queue")
 			.setRelayHost("localhost")
 			.setRelayPort(61613)
 			.setSystemLogin("admin")
