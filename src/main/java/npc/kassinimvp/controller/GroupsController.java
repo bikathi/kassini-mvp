024ca735e715bdd2e3f51c1c740cf6c1b0916c5c
Bikathi Martin <bikathimartin@outlook.com>
2023-08-14 20:05:07 +0300

    Complete endpoint for creating groupItem requests

--- src/main/java/npc/kassinimvp/controller/GroupsController.java
+++ src/main/java/npc/kassinimvp/controller/GroupsController.java
@@ -1,15 +1,23 @@
 package npc.kassinimvp.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import npc.kassinimvp.entity.AppUser;
+import npc.kassinimvp.entity.definitions.GroupItem;
+import npc.kassinimvp.payload.request.CreateGroupItemRequest;
+import npc.kassinimvp.payload.response.GenericServiceResponse;
 import npc.kassinimvp.service.GroupsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/v1/groups")
+@RequestMapping(value = "/api/v1/groups")
 @Slf4j
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class GroupsController {
@@ -22,5 +30,56 @@ public class GroupsController {
     @Value("${service.organization.name}")
     private String organizationName;
 
+    @PostMapping(value = "/create-gitem")
+    @PreAuthorize("hasAuthority('ROLE_VENDOR')")
+    public ResponseEntity<?> createGroupItem(@RequestParam String vendorId, @RequestBody CreateGroupItemRequest createGroupItemRequest) {
+        // convert the request to a proper GroupItem
+        GroupItem newGroupItem = GroupItem.builder()
+            .groupId(generateGroupItemId())
+            .groupName(createGroupItemRequest.getGroupName())
+            .groupMembers(createGroupItemRequest.getGroupMembers().stream().map(minimAppUser -> AppUser.builder()
+                .userId(minimAppUser.getUserId())
+                    .bioName(minimAppUser.getBioName())
+                    .firstName(minimAppUser.getFirstName())
+                    .lastName(minimAppUser.getLastName())
+            .build()).collect(Collectors.toList()))
+        .build();
 
+        try {
+            // add the GroupItem into the list in the Groups object in the db
+            groupsService.addNewGroupItem(vendorId, newGroupItem);
+
+            // return 200 OK if everything goes well
+            log.info("Vendor {} created new group item for their groups", vendorId);
+            return ResponseEntity.ok(new GenericServiceResponse<>(apiVersion, organizationName,
+                "Successfully created item.", HttpStatus.OK.value(), null));
+        } catch(Exception ex) {
+            log.error("Error encountered while deleting chat. Details: {}", ex.getMessage());
+            return ResponseEntity.internalServerError().body(new GenericServiceResponse<>(apiVersion, organizationName,
+                "Failed to create item. Please Try Again.", HttpStatus.INTERNAL_SERVER_ERROR.value(), null));
+        }
+    }
+
+    @PostMapping(value = "/add-gitem-member")
+    @PreAuthorize("hasAuthority('ROLE_VENDOR')")
+    public ResponseEntity<?> addGroupItemMember() {
+        return null;
+    }
+
+    @DeleteMapping(value = "/remove-gitem-member")
+    @PreAuthorize("hasAuthority('ROLE_VENDOR')")
+    public ResponseEntity<?> removeGroupItemMember() {
+        return null;
+    }
+
+    @PostMapping(value = "/sms-gitem-members")
+    @PreAuthorize("hasAuthority('ROLE_VENDOR')")
+    public ResponseEntity<?> smsGroupItemMembers() {
+        return null;
+    }
+
+    private String generateGroupItemId() {
+        return UUID.randomUUID().toString()
+            .replace("-", "").substring(0, 10);
+    }
 }
--- src/main/java/npc/kassinimvp/service/GroupsService.java
+++ src/main/java/npc/kassinimvp/service/GroupsService.java
@@ -1,11 +1,33 @@
 package npc.kassinimvp.service;
 
+import npc.kassinimvp.entity.Groups;
+import npc.kassinimvp.entity.definitions.GroupItem;
 import npc.kassinimvp.repository.GroupsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GroupsService {
     @Autowired
     GroupsRepository groupsRepository;
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    public void createGroups(Groups newGroups) {
+        groupsRepository.save(newGroups);
+    }
+
+    public void addNewGroupItem(String vendorId, GroupItem newGroupItem) {
+        Query findQuery = new Query(Criteria.where("vendorId").is(vendorId));
+        Update updateDefinition = new Update().push("groupItems", newGroupItem);
+
+        FindAndModifyOptions findAndModifyOptions = new FindAndModifyOptions().returnNew(false).upsert(false);
+        mongoTemplate.findAndModify(findQuery, updateDefinition, findAndModifyOptions, Groups.class, "vendor-groups");
+    }
 }
